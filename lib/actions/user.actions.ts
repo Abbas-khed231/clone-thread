2184dc62be880489e72d8de752cca45136f38ee6
Faisal Shaikh <faisal@heady.io>
2023-09-18 19:08:04 +0530

    Profile completed

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,5 +1,6 @@
 "use server"
 
+import Thread from "../Models/thread.model";
 import User from "../Models/user.model";
 import { connectToDB } from "../mongoose";
 import {revalidatePath} from "next/cache"
@@ -55,5 +56,30 @@ export async function fetchUser(userId: string) {
             // })
     } catch (error:any) {
         throw new Error(`Failed to fetch user: ${error.message}`);
+    }
+}
+
+export async function fetchUserPosts(userId: string) {
+    try {
+        connectToDB()
+
+        // Find all threads authored by the user with the given userId
+    const threads = await User.findOne({ id: userId })
+        .populate({
+            path: "threads",
+            model: Thread,
+            populate: {
+                path: "children",
+                model: Thread,
+                populate: {
+                path: "author",
+                model: User,
+                select: "name image id", // Select the "name" and "_id" fields from the "User" model
+                },
+            },
+        });
+        return threads;
+    } catch (error: any) {
+        throw new Error(`Failed to fetch user posts: ${error.message}`)
     }
 }

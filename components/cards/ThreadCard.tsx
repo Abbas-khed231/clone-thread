70361a3e9aa1a128b5d6e304475f12c85ca53b5a
Faisal Shaikh <faisal@heady.io>
2023-09-24 14:35:37 +0530

    ignore build error

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -36,6 +36,9 @@ const ThreadCard = ({
   comments,
   isComment,
 }: Props) => {
+
+  
+  
   return (
     <article className={`flex w-full flex-col rounded-xl ${
       isComment ? "px-0 xs:px-7" : "bg-dark-2 p-7"

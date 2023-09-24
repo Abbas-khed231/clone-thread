69e4d89ae15b3e44fab4d6c04b782d3f820641df
Faisal Shaikh <faisal@heady.io>
2023-09-24 17:12:26 +0530

    implement all features

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -2,7 +2,7 @@
 
 import * as z from "zod";
 import { useForm } from "react-hook-form";
-// import { useOrganization } from "@clerk/nextjs";
+import { useOrganization } from "@clerk/nextjs";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { usePathname, useRouter } from "next/navigation";
 
@@ -25,7 +25,7 @@ function PostThread({ userId }: {userId: string}) {
     const router = useRouter();
     const pathname = usePathname();
   
-    // const { organization } = useOrganization();
+    const { organization } = useOrganization();
   
     const form = useForm<z.infer<typeof ThreadValidation>>({
       resolver: zodResolver(ThreadValidation),
@@ -36,10 +36,11 @@ function PostThread({ userId }: {userId: string}) {
     });
   
     const onSubmit = async (values: z.infer<typeof ThreadValidation>) => {
+      
       await createThread({
         text: values.thread,
         author: userId,
-        communityId: null,
+        communityId: organization ? organization.id : null,
         path: pathname,
       });
   

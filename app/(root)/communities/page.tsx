09bd62319acd536eb82653dd250929740df84e2a
Faisal Shaikh <faisal@heady.io>
2023-09-26 14:33:16 +0530

    delete thread feature added

--- app/(root)/communities/page.tsx
+++ app/(root)/communities/page.tsx
@@ -1,39 +1,45 @@
-
-import {currentUser} from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
-import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
-import ProfileHeader from "@/components/shared/ProfileHeader";
-import { profileTabs } from "@/constants";
-import Image from "next/image";
-import ThreadsTab from "@/components/shared/ThreadsTab";
-import UserCard from "@/components/cards/UserCard";
+
+import Searchbar from "@/components/shared/SearchBar";
+import Pagination from "@/components/shared/Pagination";
+import CommnityCard from "@/components/cards/CommnityCard";
+
+import { fetchUser } from "@/lib/actions/user.actions";
 import { fetchCommunities } from "@/lib/actions/community.actions";
-import CommunityCard from "@/components/cards/CommnityCard";
 
-async function Page() {
-    const user = await currentUser();
-    if (!user) return null;
+async function Page({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) {
+  const user = await currentUser();
+  if (!user) return null;
 
-    const userInfo = await fetchUser(user.id);
-    if (!userInfo?.onboarded) redirect("/onboarding");
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
 
-    const result = await fetchCommunities({
-      searchString: "",
-      pageNumber: 1,
-      pageSize: 25
-    })
+  const result = await fetchCommunities({
+    searchString: searchParams.q,
+    pageNumber: searchParams?.page ? +searchParams.page : 1,
+    pageSize: 25,
+  });
 
-    return (
-    <section>
-      <h1 className='head-text mb-10'>Community</h1> 
+  return (
+    <>
+      <h1 className='head-text'>Communities</h1>
 
-      <div className="mt-14 flex flex-col gap-9">
+      <div className='mt-5'>
+        <Searchbar routeType='communities' />
+      </div>
+
+      <section className='mt-9 flex flex-wrap gap-4'>
         {result.communities.length === 0 ? (
-          <p className="no-result">No communities</p>
+          <p className='no-result'>No Result</p>
         ) : (
           <>
             {result.communities.map((community) => (
-              <CommunityCard 
+              <CommnityCard
                 key={community.id}
                 id={community.id}
                 name={community.name}
@@ -45,9 +51,15 @@ async function Page() {
             ))}
           </>
         )}
-      </div>
-    </section>
-  )
+      </section>
+
+      <Pagination
+        path='communities'
+        pageNumber={searchParams?.page ? +searchParams.page : 1}
+        isNext={result.isNext}
+      />
+    </>
+  );
 }
 
-export default Page
+export default Page;
